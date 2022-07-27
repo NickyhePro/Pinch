70de7e57aa7c00b5bb7c9c3cd6164a91a30750f9
Nick He <nickyhe123@gmail.com>
2022-07-27 14:03:15 +1200

    Add double tap gesture to the page image

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - Function
+    
+    private func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - Content
     
@@ -29,8 +37,10 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
+                // MARK: - Tap Gesuture
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
@@ -38,11 +48,24 @@ struct ContentView: View {
                             }
                         }
                         else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                // MARK: - Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                                
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
