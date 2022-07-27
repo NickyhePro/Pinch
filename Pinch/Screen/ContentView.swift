3779ddfcef00ddeefc6d7ce7abe47df365d072df
Nick He <nickyhe123@gmail.com>
2022-07-27 13:08:57 +1200

    Set up the new Pinch project

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Nick He on 27/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
